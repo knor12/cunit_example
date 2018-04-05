f61cd952e62586a7ca1cdabe8dba0e41ba76256a
Noreddine Kessa <knor12@yahoo.fr>
2018-04-05 18:50:27 -0400

    code clean up

--- unit_test/add.h
+++ unit_test/add.h
@@ -2,5 +2,3 @@
 #include <stdint.h>
 
 uint32_t uiAdd(uint32_t , uint32_t);
-
-
--- unit_test/test_add.c
+++ unit_test/test_add.c
@@ -8,6 +8,7 @@
 
 void test_uiAdd(){
 
+    //add asserts to the test
     CU_ASSERT(uiAdd(1,1) == 2);
     CU_ASSERT(uiAdd(0,1) == 1);
     CU_ASSERT(uiAdd(1,0) == 1);
@@ -25,12 +26,6 @@ int main(int argc , char * argv[]){
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
-
-    printf("Hello world\n");
-    
-    //CU_ASSERT(2);
-	
-
 
     return 0;
 }
